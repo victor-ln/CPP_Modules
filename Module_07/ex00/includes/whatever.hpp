e7cb7045bf257652b2c77cb4ba40ae85f6968d62
vlima-nu <vlima-nu@student.42sp.org.br>
2022-10-11 16:57:16 -0300

    feat: 🚀 ex00 done

---
+++ Module_07/ex00/includes/whatever.hpp
@@ -0,0 +1,23 @@
+/* Copyright © 2022 Victor Nunes, Licensed under the MIT License. */
+
+#ifndef MODULE_07_EX00_INCLUDES_WHATEVER_HPP_
+#define MODULE_07_EX00_INCLUDES_WHATEVER_HPP_
+
+template<typename T>
+void    swap(T& a, T& b) {
+    T   c = a;
+    a = b;
+    b = c;
+}
+
+template<typename T>
+T    max(const T& a, const T& b) {
+    return (a > b ? a : b);
+}
+
+template<typename T>
+T    min(const T& a, const T& b) {
+    return (a < b ? a : b);
+}
+
+#endif /* MODULE_07_EX00_INCLUDES_WHATEVER_HPP_ */
---
+++ Module_07/ex00/sources/main.cpp
@@ -0,0 +1,22 @@
+/* Copyright © 2022 Victor Nunes, Licensed under the MIT License. */
+
+#include <iostream>
+
+#include "../includes/whatever.hpp"
+
+int main(void) {
+    int a = 2;
+    int b = 3;
+
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    return 0;
+}

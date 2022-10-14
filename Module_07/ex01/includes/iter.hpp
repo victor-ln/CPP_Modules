4ca3abd09f29729ba217c67b62aac591b63a421a
vlima-nu <vlima-nu@student.42sp.org.br>
2022-10-14 15:47:18 -0300

    docs: 📃 Adding comments

--- Module_07/ex01/includes/iter.hpp
+++ Module_07/ex01/includes/iter.hpp
@@ -5,6 +5,13 @@
 
 #include <iostream>
 
+/**
+ * Iterate over an array of any type, and call a function on each element.
+ * 
+ * @param address The address of the first element in the array.
+ * @param end The number of elements to iterate over.
+ * @param func The function to call on each element.
+ */
 template<typename T, typename function>
 void iter(T *address, size_t end, function func) {
     for (size_t i = 0; i < end; i++) {

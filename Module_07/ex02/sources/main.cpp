7b6dc68d69630ccc17014c6988fad8899ea27ff5
vlima-nu <vlima-nu@student.42sp.org.br>
2022-10-14 15:49:27 -0300

    tests: 🔒 Adding more tests and refactoring code

--- Module_07/ex02/sources/main.cpp
+++ Module_07/ex02/sources/main.cpp
@@ -1,46 +1,119 @@
-#include <iostream>
-#include "../includes/Array.hpp"
+/* Copyright © 2022 Victor Nunes, Licensed under the MIT License. */
+
 #include <cstdlib>
-#include <vector>
+#include <iostream>
 
-// #define MAX_VAL 750
-#define MAX_VAL 10
+#include "../includes/Array.hpp"
+
+#define MAX_VAL     750
+#define MAX_STRS    5
+
+static void     initIntArray(Array<int>& numbers, int *nbMirror);
+
+template<typename T>
+static void    testIfIsInitialized(const Array<T>& array);
+
+template<typename T>
+static void    testInvalidAccess(const Array<T>& array);
+
+template<typename T>
+static void    testIsDeepCopying(const Array<T>& array);
+
+template<typename T>
+static bool    testIfValuesAreTheSame(const Array<T>& array, T* srcArray);
+
+
 int main(void) {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
+    Array<int>          numbers(MAX_VAL);
+    Array<std::string>  strings(MAX_STRS);
 
+    int         *nbMirror = new int[MAX_VAL];
+    std::string sMirror[MAX_STRS] = {"Hello", "World", "from", "C++", "!"};
+
+    testIfIsInitialized(numbers);
+    testIfIsInitialized(strings);
+    initIntArray(numbers, nbMirror);
+    for (int i = 0; i < MAX_STRS; i++)
+        strings[i] = sMirror[i];
+    if (!testIfValuesAreTheSame<int>(numbers, nbMirror))
+        std::cout << "didn't save the same value!!\n\n";
+    if (!testIfValuesAreTheSame<std::string>(strings, sMirror))
+        std::cout << "didn't save the same value!!\n\n";
+    testInvalidAccess(numbers);
+    testInvalidAccess(strings);
+    for (int i = 0; i < MAX_VAL; i++)
+        numbers[i] = rand();
+    delete [] nbMirror;
+    return 0;
+}
+
+/**
+ * It initializes two arrays of integers with random values
+ * 
+ * @param numbers The class array<int> to initialize
+ * @param nbMirror An array that will be used to check 
+ * that all values have been stored correctly.
+ */
+static void     initIntArray(Array<int>& numbers, int *nbMirror) {
     srand(time(NULL));
     for (int i = 0; i < MAX_VAL; i++) {
         const int value = rand();
         numbers[i] = value;
-        mirror[i] = value;
+        nbMirror[i] = value;
     }
+}
 
-    // //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
+template<typename T>
+static void    testIfIsInitialized(const Array<T>& array) {
+    std::cout << "\tTesting if is initialized by default\n\n";
+    std::cout << array[0] << '\n';
+    std::cout << array[array.size() - 1] << '\n';
+    std::cout << '\n';
+}
+
+
+/**
+ * It tries to access an element of the array that is out of bounds, and catches
+ * the exception that is thrown
+ */
+template<typename T>
+static void    testInvalidAccess(const Array<T>& array) {
+    try {
+        std::cout << array[-2] << '\n';
+    } catch(const std::exception& e) {
+        std::cout << e.what() << '\n';
     }
-
-    for (int i = 0; i < MAX_VAL; i++) {
-        if (mirror[i] != numbers[i]) {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return (1);
-        }
+    try {
+        std::cout << array[array.size()] << '\n';
+    } catch(const std::exception& e) {
+        std::cout << e.what() << '\n';
     }
-    // try {
-    //     numbers[-2] = 0;
-    // } catch(const std::exception& e) {
-    //     std::cerr << e.what() << '\n';
-    // }
-    // try {
-    //     numbers[MAX_VAL] = 0;
-    // } catch(const std::exception& e) {
-    //     std::cerr << e.what() << '\n';
-    // }
+}
 
-    // for (int i = 0; i < MAX_VAL; i++)
-    //     numbers[i] = rand();
-    delete [] mirror;//
-    return 0;
+/**
+ * It tests if the copy constructor and assignment operator is deep copying
+ */
+template<typename T>
+static void    testIsDeepCopying(const Array<T>& array) {
+    std::cout << "\tTesting if is deep copying\n\n";
+    Array<T> tmp1 = array;
+    Array<T> test1(tmp1);
+    std::cout << '\n';
+}
+
+/**
+ * It tests if the values in the array are the same as the values in the source
+ * array
+ * 
+ * @param array The array to test.
+ * @param srcArray The source array to copy from.
+ * 
+ * @return If they are the same, it returns true, otherwise it returns false.
+ */
+template<typename T>
+static bool    testIfValuesAreTheSame(const Array<T>& array, T* srcArray) {
+    for (size_t i = 0; i < array.size(); i++)
+        if (srcArray[i] != array[i])
+            return (false);
+    return (true);
 }
